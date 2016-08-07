c92e449fcfe8bcf29ad315978e712d24d6a195bf
HOANGNH <hoangnhonline@gmail.com>
2016-08-07 16:39:14 +0700

    config

--- config/nghien.php
+++ config/nghien.php
@@ -10,7 +10,7 @@ return [
     'num_alert' => 10, // number rows for alert on top menu
     'upload_path' => public_path() . '/uploads/', // media_upload_path   
     'upload_url' => $url . '/uploads/', // image path,
-    'max_size_upload' => 8000000    
+    'max_size_upload' => 8000000 
 ];
 
 ?>
