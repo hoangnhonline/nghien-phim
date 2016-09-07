5e7d905a2064b76a875fad187713310eb7e70341
HOANGNH <hoangnhonline@gmail.com>
2016-09-08 06:52:57 +0700

    update

--- resources/views/layout/frontend.blade.php
+++ resources/views/layout/frontend.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="vi" lang="vi">
    <head>
       <title>@yield('title')</title>
       <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
