82283ed37a6f9f470cbef2c5c6280257faf6b47f
HoangNH <hoangnhonline@gmail.com>
2016-10-11 21:37:32 +0700

    detail

--- resources/views/layout/detail.blade.php
+++ resources/views/layout/detail.blade.php
@@ -479,6 +479,74 @@ $(document).ready(function(){
 });
 
 </script>
+<script type="text/javascript">
+   
+  window.fbAsyncInit = function() {
+  FB.init({
+    appId      : $('#fb-app-id').val(),
+    cookie     : true,  // enable cookies to allow the server to access
+                        // the session
+    xfbml      : true,  // parse social plugins on this page
+    version    : 'v2.7' // use graph api version 2.7
+  });
+
+  // Now that we've initialized the JavaScript SDK, we call
+  // FB.getLoginStatus().  This function gets the state of the
+  // person visiting this page and can return one of three states to
+  // the callback you provide.  They can be:
+  //
+  // 1. Logged into your app ('connected')
+  // 2. Logged into Facebook, but not your app ('not_authorized')
+  // 3. Not logged into Facebook and can't tell if they are logged into
+  //    your app or not.
+  //
+  // These three cases are handled in the callback function.
+
+  // FB.getLoginStatus(function(response) {
+  //   statusChangeCallback(response);
+  // });
+};
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+$(document).ready(function() {
+  $.ajaxSetup({
+      headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+      }
+  });
+  $('.login-by-facebook-popup').click(function() {
+    FB.login(function(response){
+      if(response.status == "connected")
+      {
+         // call ajax to send data to server and do process login
+        var token = response.authResponse.accessToken;
+        $.ajax({
+          url: $('#route-ajax-login-fb').val(),
+          method: "POST",
+          data : {
+            token : token
+          },
+          success : function(data){
+            
+            location.reload();
+            
+          }
+        });
+
+      }
+    }, {scope: 'public_profile,email'});
+  });
+});
+</script>
+<input type="hidden" id="route-ajax-login-fb" value="{{ route('ajax-login-by-fb') }}">
+<input type="hidden" id="fb-app-id" value="{{ env('FACEBOOK_APP_ID') }}">
 </body>
 </html>
 
